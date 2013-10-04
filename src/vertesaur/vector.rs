c6b31fe38aeca039ff145a213cb11c503330e23f
Aaron Dandy <aaron.dandy@live.com>
2013-10-03 23:47:44 -0600

    some very basic num/ops impl for vectors

--- src/vertesaur/vector.rs
+++ src/vertesaur/vector.rs
@@ -18,7 +18,28 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-macro_rules! vec_op_impl(($t:ident $i:ident $f:ident $($c:ident),*) => (
+use std::num::{Zero};
+
+macro_rules! make_vector_struct(($t:ident $($c:ident),*) => (pub struct $t<T> { $($c: T),*}))
+
+macro_rules! vec_zero_impl(($t:ident $($c:ident),*) => (
+	impl<T:Zero> Zero for $t<T>{
+		fn zero() -> $t<T> {
+			$t{
+				$(
+					$c: Zero::zero(),
+				)*
+			}
+		}
+
+		fn is_zero(&self) -> bool {
+			false // I have no idea how to compare values!
+		}
+	}
+))
+
+// simple component to vector operator abstractions
+macro_rules! vec_simple_op_impl(($t:ident $i:ident $f:ident $($c:ident),*) => (
 	impl<T:$i<T,T>> $i<$t<T>,$t<T>> for $t<T> {
 		#[inline] fn $f(&self, rhs: &$t<T>) -> $t<T> {
 			$t{
@@ -30,25 +51,51 @@ macro_rules! vec_op_impl(($t:ident $i:ident $f:ident $($c:ident),*) => (
 	}
 ))
 
-macro_rules! build_vectors(($({$t:ident $($c:ident),*})|*) => (
-	mod generated{$(
-		pub struct $t<T> { $($c: T,)*}
-		vec_op_impl!($t Add add $($c),*)
-		vec_op_impl!($t Sub sub $($c),*)
-	)*}
+// scalar multiplication
+macro_rules! vec_scalar_mul_op_imp(($t:ident $($c:ident),*) => (
+	impl<T:Mul<T,T>> Mul<T,$t<T>> for $t<T> {
+		#[inline] fn mul(&self, rhs: &T) -> $t<T> {
+			$t{
+				$(
+					$c: self.$c.mul(rhs),
+				)*
+			}
+		}
+	}
 ))
 
+// for now this is going to be expanded out just using dumb copy & paste
+// later when rust #4375 is fixed this can be cleaned up
+// see: https://github.com/mozilla/rust/issues/4375
 
-build_vectors!(
-	{Vector2 x,y}
-	|{Vector3 x,y,z}
-	|{Vector4 x,y,z,w}
-)
+make_vector_struct!(Vector1 x)
+vec_simple_op_impl!(Vector1 Add add x)
+vec_simple_op_impl!(Vector1 Sub sub x)
+vec_scalar_mul_op_imp!(Vector1 x)
+
+make_vector_struct!(Vector2 x,y)
+vec_simple_op_impl!(Vector2 Add add x,y)
+vec_simple_op_impl!(Vector2 Sub sub x,y)
+vec_scalar_mul_op_imp!(Vector2 x,y)
+
+make_vector_struct!(Vector3 x,y,z)
+vec_simple_op_impl!(Vector3 Add add x,y,z)
+vec_simple_op_impl!(Vector3 Sub sub x,y,z)
+vec_scalar_mul_op_imp!(Vector3 x,y,z)
+
+make_vector_struct!(Vector4 x,y,z,w)
+vec_simple_op_impl!(Vector4 Add add x,y,z,w)
+vec_simple_op_impl!(Vector4 Sub sub x,y,z,w)
+vec_scalar_mul_op_imp!(Vector4 x,y,z,w)
+
+vec_zero_impl!(Vector2 x,y)
 
 #[cfg(test)]
 mod tests {
-	use super::generated::*;
+	use std::num::Zero;
+	use super::*;
 
+	// only testing Vector2 here for most operations
 	// the thinking here is that as the impls are generated from macros...
 	// if it works for Vector2 it should work for Vector4
 
@@ -56,6 +103,24 @@ mod tests {
 	fn add_vector2_verification() {
 		let v = Vector2{x:1i,y:2i} + Vector2{x:3i,y:5i};
 		assert_eq!((v.x,v.y),(4i,7i));
+	}
+
+	#[test]
+	fn sub_vector2_verification() {
+		let v = Vector2{x:1i,y:5i} - Vector2{x:3i,y:2i};
+		assert_eq!((v.x,v.y),(-2i,3i));
+	}
+
+	#[test]
+	fn scalar_mul_vector2_verification(){
+		let v = Vector2{x:1_f64,y: -3_f64} * 3_f64;
+		assert_eq!((v.x,v.y),(3_f64,-9_f64));
+	}
+
+	#[test]
+	fn zero_vector_verification(){
+		let v : Vector2<int> = Zero::zero();
+		assert_eq!((v.x,v.y),(0i,0i));
 	}
 
 }
